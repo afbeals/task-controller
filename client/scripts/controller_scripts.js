93be203716c6eea94662b14f1012525a30e90226
Allan <afbeals@hotmail.com>
2016-12-19 19:16:17 -0800

    add duration calculation when creating task

--- client/scripts/controller_scripts.js
+++ client/scripts/controller_scripts.js
@@ -8,22 +8,25 @@ TCommander.controller('nav_controller',['$scope', 'task_factory', function ($sco
 TCommander.controller('task_controller',['$scope', 'task_factory',function($scope, task_factory){
 	//initialize obj to maintain consistent data type
 	$scope.task = {};
-	$scope.currentTasks = [];
+	$scope.routine = [];
 	$scope.dynForm = {};
 	$scope.address = {};
+
+	//initialize variables
+	
 
 	//unused
 	//check if routine exist in session storage, if so run callback with routine data (unused)
 	var routine_check = function(data,callback){
-		if(!sessionStorage.currentRoutine){
-			sessionStorage.setItem('currentRoutine', JSON.stringify(data));
-		}else if(sessionStorage.currentRoutine){
+		if(!sessionStorage.currentroutine){
+			sessionStorage.setItem('currentroutine', JSON.stringify(data));
+		}else if(sessionStorage.currentroutine){
 			callback(data);
 		}
 	};
 	//run routine_check and pass task obj, then run factory with passed info
-	$scope.createRoutine = function(){
-		routine_check($scope.task, function(data){task_factory.addTaskToRoutine(data)});
+	$scope.createroutine = function(){
+		routine_check($scope.task, function(data){task_factory.addTaskToroutine(data)});
 	}
 
 
@@ -38,8 +41,8 @@ TCommander.controller('task_controller',['$scope', 'task_factory',function($scop
 
 	//Controller Methods
 	//pass routine (array of obj) to factory, to get sent to BE
-	$scope.createRoutine = function(){
-		task_factory.createRoutine($scope.currentTasks);
+	$scope.createroutine = function(){
+		task_factory.createroutine($scope.routine);
 
 	};
 	//clear form values
@@ -79,12 +82,11 @@ TCommander.controller('task_controller',['$scope', 'task_factory',function($scop
     $scope.map = new google.maps.Map(document.getElementById('map'), $scope.mapOptions);
 
     //Google Distance Matrix info
-    //create scope function pass form data in function for calculation
-	$scope.getNewDistance = function(origina,destination1){
-		console.log($scope.address);
+	//create scope function pass form data to function for destination calculation
+	getNewDistance = function(origina,destination1){
 		var service = new google.maps.DistanceMatrixService();
 		service.getDistanceMatrix(
-		  {
+		{
 		    origins: [origina],
 		    destinations: [destination1],
 		    travelMode: 'DRIVING'
@@ -93,52 +95,71 @@ TCommander.controller('task_controller',['$scope', 'task_factory',function($scop
 		    // unitSystem: UnitSystem,
 		    // avoidHighways: Boolean,
 		    // avoidTolls: Boolean,
-		  }, callback);
-
-		function callback(response, status) {
-		  if (status == 'OK') {
-		    var origins = response.originAddresses;
-		    var destinations = response.destinationAddresses;
-		    console.log(response.rows[0].elements[0].duration.text);
-		    console.log(response.rows[0].elements[0].duration.value);
-		    for (var i = 0; i < origins.length; i++) {
-		      var results = response.rows[i].elements;
-		      for (var j = 0; j < results.length; j++) {
-		        var element = results[j];
-		        var distance = element.distance.text;
-		        var duration = element.duration.text;
-		        var from = origins[i];
-		        var to = destinations[j];
-		      }
-		    }
-		  }
-		}
+		}, distanceMatrixResponse);
+	}
+	//distance matrix response function
+    var distanceMatrixResponse = function(response, status) 
+    {
+	  if (status == 'OK') {
+	    var origins = response.originAddresses;
+	    var destinations = response.destinationAddresses;
+	    //add task duration to task (get ready to submit to database)
+	    $scope.task.task_duration=Math.floor(response.rows[0].elements[0].duration.value / 60);
+	    //clear task for future use
+	    $scope.task = {};
+	    // for (var i = 0; i < origins.length; i++) {
+	    //   var results = response.rows[i].elements;
+	    //   for (var j = 0; j < results.length; j++) {
+	    //     var element = results[j];
+	    //     var distance = element.distance.text;
+	    //     var duration = element.duration.text;
+	    //     var from = origins[i];
+	    //     var to = destinations[j];
+	    //   }
+	    // }
+	  }
 	}
 
 	//sessionStorage 
 	//loop through sessionStorage to get currently stored task
 	var get_session_task = function(){
 		for(task in sessionStorage){
-			$scope.currentTasks.push(JSON.parse(sessionStorage[task]));
+			$scope.routine.push(JSON.parse(sessionStorage[task]));
 		}
 	}
 	
+	var testHome = "Portland, OR";
+
 	//scope task method
 	$scope.addTaskToSesRoutine = function(){
 		//grab task_name as identifier
 		var task_name = $scope.task.task_name;
 		var session_task_name = "task:"+task_name;
+		//compute length of routine
+		var size = Object.keys(sessionStorage).length;
 		//check if already exist, if it does then alert the user
 		 if(!sessionStorage[session_task_name]){
-		 	//set each task by 'task: "task_name"' with object information
-		 	sessionStorage.setItem(session_task_name,JSON.stringify($scope.task));
-		 	//push new task to array to update printed list
-		 	$scope.currentTasks.push(JSON.parse(sessionStorage[session_task_name]));
+		 	//if first item in list then use home as origin location
+		 	if(sessionStorage[session_task_name] == sessionStorage[Object.keys(sessionStorage)[0]]){
+				//set each task by 'task: "task_name"' with object information
+			 	sessionStorage.setItem(session_task_name,JSON.stringify($scope.task));
+			 	//push new task to array to update printed list
+			 	$scope.routine.push(JSON.parse(sessionStorage[session_task_name]));
+			 	//run distance matrix to get duration in seconds
+				getNewDistance(testHome, $scope.task.task_location);
+			}else{
+				//set each task by 'task: "task_name"' with object information
+			 	sessionStorage.setItem(session_task_name,JSON.stringify($scope.task));
+			 	//push new task to array to update printed list
+			 	$scope.routine.push(JSON.parse(sessionStorage[session_task_name]));
+			 	//run distance matrix to get duration in seconds
+				getNewDistance(sessionStorage[Object.keys(sessionStorage)[size - 1]], $scope.task.task_location);
+			}
 		 } else {
 		 	alert('sorry '+$scope.task.task_name+' already exist, but you can still remove, edit, or use a different name!')
 		 }
 		 //clear task afterwards for next task
-		 $scope.task={};
+		 //$scope.task={};
 	};
 	
 	//remove task from routine list
@@ -149,23 +170,21 @@ TCommander.controller('task_controller',['$scope', 'task_factory',function($scop
 			//remove from sessionStorage
 			sessionStorage.removeItem("task:"+name);
 			//and remove from array to update front end
-			$scope.currentTasks.splice(name, 1);
+			$scope.routine.splice(name, 1);
 		}else{
 			alert('hmm something went wrong, please reload the page and try again.')
 		}
 	};
 	//remove all task from routine
 	$scope.removeAllTask = function(){
-		console.log($scope.currentTasks,"sesStor",sessionStorage)
-		if($scope.currentTasks.length > 1){
+		if($scope.routine.length > 1){
 			if (confirm('Are you sure you want to clear your task?')){
-			    console.log("yeuyspe");
 			    sessionStorage.clear();
-			    $scope.currentTasks=[];
+			    $scope.routine=[];
 			}
 		}else{
 			sessionStorage.clear();
-			$scope.currentTasks=[];
+			$scope.routine=[];
 		}
 	}
 	//run initial functions for SPA
