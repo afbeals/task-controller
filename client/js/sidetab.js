3af68bc1e5519452e6add633c0ee22b4f907426e
Allan <afbeals@hotmail.com>
2017-01-15 19:27:59 -0800

    'sidetab'

--- client/js/sidetab.js
+++
@@ -1,7 +0,0 @@
-console.log("loaded");
-
-$('#openSide').on('click',function(){
-    $('.createRoutine #sideTab .prefillOptions').css('display','block');
-    $('#sideTab').css('width','250px');
-
-})
